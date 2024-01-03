dfdc69f75afbe617c2aee57c3c34248003f945ba
Roman <feldman.roman@gmail.com>
2024-01-03 15:11:48 -0800

    bump dependency

--- vim/init.lua
+++ vim/init.lua
@@ -125,7 +125,7 @@ require("lazy").setup({
   -- fuzzy finder framework
   {
     "nvim-telescope/telescope.nvim", 
-    tag = '0.1.1',
+    tag = '0.1.4',
     dependencies = { 
       "nvim-lua/plenary.nvim" ,
       "nvim-treesitter/nvim-treesitter",
